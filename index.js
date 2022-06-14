493d62b2a41903ed5eee32fe911f39c14bef0875
Henrique Lopes <h.lopes.silva2015@gmail.com>
2022-06-14 17:54:45 -0300

    Update - Paths return

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const build = require("./src/build");
+const createJson = require("./src/code/createJson");
+const getSettings = require("./src/code/getSettings");
+const getSource = require("./src/code/getSource");
+const listFiles = require("./src/code/listFiles");
 
-function run(){
-  build()
+// function run(){
+//   build()
+// }
+
+async function run(){
+  const { search } = await getSettings();
+  const source = getSource(search.path);
+  const fileList = await listFiles(source);
+
+  return new Promise((resolve) => {
+    resolve(createJson(fileList));
+  });
 }
 
 run()
--- src/code/listFiles.js
+++ src/code/listFiles.js
@@ -1,28 +1,57 @@
 const fs = require("fs");
 const getSettings = require("./getSettings");
+const path = require('path')
 
-module.exports = async function listFiles(dir, file = [], dirFile = "") {
+
+module.exports = async function listFiles(dir) {
+  const dataResult = []
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
   }
 
   let listOfFiles = fs.readdirSync(dir);
 
-  for (let index in listOfFiles) {
-    let stat = fs.statSync(dir + "/" + listOfFiles[index]);
-    const { search } = await getSettings();
-    const regexFile = new RegExp(search.file);
+  listOfFiles.map(async (item) => {
+    const stat = fs.statSync(dir + "/" + item);
 
-    if (stat.isDirectory()) {
-      listFiles(`${dir}/${listOfFiles[index]}`, file, listOfFiles[index]);
-    } else if (regexFile.exec(listOfFiles[index])) {
-      file.push({
-        name: listOfFiles[index],
-        path: `${dir}/${listOfFiles[index]}`,
-        relativePath: dirFile,
-      });
+    if(stat.isDirectory()){
+      dataResult.push({
+        id: `${dir}/${item}`,
+        name: item,
+        subs: await listFiles(`${dir}/${item}`)
+      })
+    }else {
+      dataResult.push({
+        teste: ''
+      })
     }
-  }
 
-  return file;
+    return null
+  })
+
+  // for (let index in listOfFiles) {
+  //   let stat = fs.statSync(dir + "/" + listOfFiles[index]);
+  //   const { search } = await getSettings();
+  //   const regexFile = new RegExp(search.file);
+
+  //   if (stat.isDirectory()) {
+  //     file.push({
+  //       name: listOfFiles[index],
+  //       path: `${dir}/${listOfFiles[index]}`,
+  //       subs: listFiles(
+  //         `${dir}/${listOfFiles[index]}`,
+  //         file,
+  //         listOfFiles[index]
+  //       ),
+  //     });
+  //   } else if (regexFile.exec(listOfFiles[index])) {
+  //     file.push({
+  //       name: listOfFiles[index],
+  //       path: `${dir}/${listOfFiles[index]}`,
+  //     });
+  //   }
+  // }
+
+
+  return dataResult;
 };
